bd1c5cea657961506b92085b86b50a8390e1a241
Manu Bhardwaj <manu@bhardwaj.info>
2018-06-12 15:55:03 -0400

    Blink Health 2

--- python/blink/tree.py
+++ python/blink/tree.py
@@ -13,11 +13,19 @@ inputs = [
 s = {}
 
 for i in inputs:
+    # Tokenize the inputs into a list, e.g. "/drugs/A" becomes ['drugs','A']
     i = i.split('/')
     subtree = s
+
+    # Go from left to right in the list, creating a dict in each case
     for dirname in i:
+
+        # Create the dict of the current element
         if dirname not in subtree:
             subtree[dirname] = {}
+
+        # Now go deeper into the list, to recursively construct subdicts
         subtree = subtree[dirname]
 
+# Pretty printing for testing
 #print(yaml.dump(s))
